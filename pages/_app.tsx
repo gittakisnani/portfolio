90786f951ae5afbf76b4d198f4ae3681a65a7358
Medwebdev <mtakisnani09@gmail.com>
2022-10-03 22:01:31 +0100

    Changed icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import ScrollButton from '../components/ScrollButton'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
       <>
+      <Head>
+      <link rel="icon" href="/favicon.ico" />
+      </Head>
         <ScrollButton />
         <Component {...pageProps} />
       </>
